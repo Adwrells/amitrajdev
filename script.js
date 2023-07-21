9acd244be508c99df4e90ab2b7af78bc8d74dbd4
Adwrells <Adwrells@gmail.com>
2023-07-21 11:41:17 +0545

    date BS

--- script.js
+++ script.js
@@ -1,57 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const bsMonthDays = [31, 32, 31, 32, 31, 30, 30, 30, 29, 29, 30, 30]; // Days in each month of the Bikram Samvat (BS) calendar
-
   // Function to convert Gregorian date to Bikram Samvat (BS) date
-  function convertToBSDate(gYear, gMonth, gDate) {
-    const bsYearDiff = 57;
+  function convertToBSDate(date) {
+    const bsYearDiff = 56;
     const bsMonthDiff = 8;
     const bsDateDiff = 17;
 
-    let bsYear = gYear + bsYearDiff;
-    let bsMonth = gMonth + bsMonthDiff - 1; // Month index starts from 0
-    let bsDate = gDate + bsDateDiff;
+    const gYear = date.getFullYear();
+    const gMonth = date.getMonth();
+    const gDate = date.getDate();
 
-    while (bsDate > bsMonthDays[bsMonth]) {
-      bsDate -= bsMonthDays[bsMonth];
+    let bsYear = gYear + bsYearDiff;
+    let bsMonth = gMonth + bsMonthDiff;
+    if (gDate >= bsDateDiff) {
       bsMonth += 1;
-      if (bsMonth >= 12) {
-        bsMonth -= 12;
-        bsYear += 1;
-      }
+      bsDate = gDate - bsDateDiff;
+    } else {
+      bsDate = 30 - (bsDateDiff - gDate);
+    }
+
+    if (bsMonth > 12) {
+      bsMonth -= 12;
+      bsYear += 1;
     }
 
     return {
       year: bsYear,
-      month: bsMonth + 1, // Adding 1 to adjust month index back to 1-based
+      month: bsMonth,
       date: bsDate
     };
   }
 
-  // Function to calculate the difference in days between two dates
-  function dateDifference(gYear, gMonth, gDate, bsYear, bsMonth, bsDate) {
-    const gDateObj = new Date(gYear, gMonth, gDate);
-    const bsDateObj = new Date(bsYear, bsMonth, bsDate);
-    const difference = Math.floor((bsDateObj - gDateObj) / (1000 * 60 * 60 * 24));
-    return difference;
-  }
-
-  // Function to update the date difference on the webpage
-  function updateDateDifference() {
+  // Function to update the date and time in Bikram Samvat (BS)
+  function updateDateTime() {
     const currentDate = new Date();
-    const gYear = currentDate.getFullYear();
-    const gMonth = currentDate.getMonth();
-    const gDate = currentDate.getDate();
+    const bsDate = convertToBSDate(currentDate);
 
-    const bsDate = convertToBSDate(gYear, gMonth, gDate);
     const bsFormattedDate = `${bsDate.year}-${bsDate.month}-${bsDate.date}`;
 
-    const bsDateString = `Today's BS date is ${bsFormattedDate}`;
-    document.getElementById("dateDifference").textContent = bsDateString;
+    let bsFormattedTime = `${currentDate.getHours() % 12}:${String(currentDate.getMinutes()).padStart(2, '0')}:${String(currentDate.getSeconds()).padStart(2, '0')}`;
+    bsFormattedTime += currentDate.getHours() >= 12 ? ' PM' : ' AM';
 
-    const diff = dateDifference(gYear, gMonth, gDate, bsDate.year, bsDate.month - 1, bsDate.date);
-    document.getElementById("dateDifference").textContent += `<br>Difference in days: ${diff}`;
+    const bsDateTimeString = `BS ${bsFormattedDate} ${bsFormattedTime}`;
+    document.getElementById("currentDateTime").textContent = bsDateTimeString;
   }
 
-  // Call the function to update the display
-  updateDateDifference();
+  // Call the function every second to update the time
+  setInterval(updateDateTime, 1000);
 });
