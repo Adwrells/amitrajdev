0a432497ea8f9aa54d7664e875aef6d53eec27ca
Adwrells <Adwrells@gmail.com>
2023-07-21 08:09:18 +0545

    BS date

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to update the date and time
-    function updateDateTime() {
-      const dateTime = new Date();
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-      const formattedDateTime = dateTime.toLocaleDateString('en-US', options);
-      document.getElementById("currentDateTime").textContent = formattedDateTime;
+  // Function to convert Gregorian date to Bikram Samvat (BS) date
+  function convertToBSDate(date) {
+    const bsYearDiff = 57;
+    const bsMonthDiff = 8;
+
+    const gYear = date.getFullYear();
+    const gMonth = date.getMonth();
+    const gDate = date.getDate();
+
+    let bsYear = gYear + bsYearDiff;
+    let bsMonth = gMonth + bsMonthDiff;
+    if (bsMonth >= 12) {
+      bsMonth -= 12;
+      bsYear += 1;
     }
-  
-    // Call the function every second to update the time
-    setInterval(updateDateTime, 1000);
-  });
-  
+
+    return {
+      year: bsYear,
+      month: bsMonth,
+      date: gDate
+    };
+  }
+
+  // Function to update the date and time in Bikram Samvat (BS)
+  function updateDateTime() {
+    const currentDate = new Date();
+    const bsDate = convertToBSDate(currentDate);
+
+    const bsFormattedDate = `${bsDate.year}-${bsDate.month + 1}-${bsDate.date}`;
+
+    let bsFormattedTime = `${currentDate.getHours() % 12}:${String(currentDate.getMinutes()).padStart(2, '0')}:${String(currentDate.getSeconds()).padStart(2, '0')}`;
+    bsFormattedTime += currentDate.getHours() >= 12 ? ' PM' : ' AM';
+
+    const bsDateTimeString = `BS ${bsFormattedDate} ${bsFormattedTime}`;
+    document.getElementById("currentDateTime").textContent = bsDateTimeString;
+  }
+
+  // Call the function every second to update the time
+  setInterval(updateDateTime, 1000);
+});
